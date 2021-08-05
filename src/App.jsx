2adec56341fa449394b24de6d60fb8d32a6c98de
Celia Lobet <lobet.c@gmail.com>
2021-08-05 17:57:02 +0200

    added localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 import { v4 as uuid } from "uuid";
 import "./App.css";
 import Sidebar from "components/Sidebar";
 import NoteDisplay from "components/NoteDisplay";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
   const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   const onAddNote = () => {
     const newNote = {
